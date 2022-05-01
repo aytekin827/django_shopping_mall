479382ea0282f821d1768ec51108142abbbfbbbb
aytekin_desktop <mjeong827@gmail.com>
2022-05-01 22:52:59 +0900

    drf설치, serializer만들기

---
+++ fc_django/product/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Product
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        field = '__all__' # 이렇게 하면 자동으로 모델 안에 있는 모든 필드들을 가져온다.
