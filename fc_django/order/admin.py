97ecae28a1c49104216093fcee4a068557350836
aytekin_desktop <mjeong827@gmail.com>
2022-05-03 17:57:27 +0900

    admin페이지 필터와 커스텀필드

--- fc_django/order/admin.py
+++ fc_django/order/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
+from django.utils.html import format_html # html escape를 없애준다.
 from .models import Order
 # Register your models here.
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('fcuser', 'product')
+    list_display = ('fcuser', 'product', 'styled_status')
+    list_filter = ('status',) # 필터 넣어주는 방법
+
+    def styled_status(self, obj):
+        if obj.status == '환불':
+            return format_html(f'<span style="color:red"><strong>{obj.status}</strong></span>')
+        elif obj.status == '결제완료':
+            return format_html(f'<span style="color:green"><strong>{obj.status}</strong></span>')
+        else:
+            return obj.status
+    print(styled_status.__dict__)
+    print(dir(styled_status))
+
+    styled_status.short_description = '상태'
 
 admin.site.register(Order, OrderAdmin)
